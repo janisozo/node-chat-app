9342287f5145acca83b0e9bde2e0d71213ccdc9e
janisozo <todlebensjanis@gmail.com>
2018-08-05 08:57:37 -0700

    Enabled message broadcasting

--- public/js/index.js
+++ public/js/index.js
@@ -3,16 +3,6 @@ var socket = io();
 socket.on('connect', function () {
 	console.log('Connected to server!');
 
-	// socket.emit("createEmail", {
-	// 	to: "Wife@gmail.com",
-	// 	text: "I love my wife",
-	// 	timestamp: 123568
-	// });
-
-	socket.emit('createMessage', {
-		from: "OzyBro",
-		text: 'Only if we meet at your place ;)'
-	});
 });
 
 
@@ -24,7 +14,3 @@ socket.on('disconnect', function () {
 socket.on('newMessage', function (message) {
 	console.log('message:', message)
 });
-
-// socket.on("newEmail", function (email) {
-// 	console.log("New email received", email);
-// });
--- server/server.js
+++ server/server.js
@@ -15,25 +15,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log("New user connected!");
 
-	socket.emit('newMessage', {
-		from: "LaimaPuff",
-		text: "Hey. Who wants to meet up at zix? :3",
-		createdAt: new Date().toLocaleTimeString()
-	});
-
 	socket.on('createMessage', (message) => {
 		console.log('Message: ', message, new Date().toLocaleTimeString());
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().toLocaleTimeString()
+		});
 	});
-
-	// socket.emit('newEmail', {
-	// 	from: "toto@inbox.lv",
-	// 	text: "I got a bad ass new bike!",
-	// 	receivedAt: "1236534"
-	// });
-
-	// socket.on("createEmail", (newEmail) => {
-	// 	console.log("New email created:", newEmail);
-	// });
 
 	socket.on("disconnect", () => {
 		console.log("A client disconnected");
