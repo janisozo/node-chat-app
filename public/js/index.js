8db7a7e2810ff6cb373ed34c5d74fcd2a82809af
janisozo <todlebensjanis@gmail.com>
2018-08-09 10:13:19 -0700

    Added autoscroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io();
 
+function scrollToBottom () {
+	var messages = jQuery('#messages');
+	var newMessage = messages.children('li:last-child');
+
+	var clientHeight = messages.prop('clientHeight');
+	var scrollHeight = messages.prop('scrollHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+
+	if (scrollTop + clientHeight + newMessageHeight + lastMessageHeight >= scrollHeight) {
+		messages.scrollTop(scrollHeight);
+	};
+};
+
 socket.on('connect', function () {
 	console.log('Connected to server!');
 
@@ -24,6 +39,7 @@ socket.on('newMessage', function (message) {
 		createdAt: formattedTime
 	});
 	jQuery('#messages').append(html);
+	scrollToBottom();
 });
 
 socket.on('newLocationMessage', (locationMessage) => {
@@ -43,6 +59,7 @@ socket.on('newLocationMessage', (locationMessage) => {
 		createdAt: formattedTime
 	});
 	jQuery('#messages').append(html);
+	scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
